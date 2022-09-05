18262e2eb99705ebb220854204f843a9ec710272
Augusto0414 <augustopelis4@gmail.com>
2022-09-05 11:16:46 -0500

    Generate pokemon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import Character from './components/Character';
 
-function App() {
+function App() {  
+  const [character, setCharacter] = useState({});
+
+  const generatePokeId = () => {
+    let min = Math.ceil(1);
+    let max = Math.floor(898);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  } 
+  
+  async function pokeApi (){
+    const pokeId = generatePokeId();
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`);
+    const data = await response.json();
+    setCharacter(data);
+  }
+
+  useEffect(() => {
+    pokeApi();
+  }, []); 
+ 
   return (
     <div className="App">
+     {
+        <Character 
+        id = {character.id}
+        name = {character.name}
+        />
+     }
     </div>
-  );
+  )
 }
 
 export default App;
+ 
---
+++ src/components/Character.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../style/Character.css'
+
+
+const Character = ({id, name}) => {
+	
+  return (
+    < div className ='cards d-flex justify-content-center'>
+				<div className ='card' style ={{width: '30rem'}} key={id}>
+					<div className ='card-header'>
+					   <h2 className ='card-title'>{name} </h2>
+					</div>
+					<div className ='card-body'>
+						 <img src = {"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + id + ".png"} alt ='pokemon'/>
+						    <p className ='card-text'> {''}</p>
+					</div>
+					<div className ='card-footer'>
+						 <button className ='btn btn-primary'>Generate new pokemon</button>
+					</div>	
+				</div>
+	</div>
+
+  )
+}
+
+export default Character
